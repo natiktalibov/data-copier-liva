42204f69f25df938147f41b6f3060cf067e68b4f
oracle <oracle@hadoop.example.com>
2021-10-23 15:18:18 +0200

    Initial Commit verison

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-
 from config import DB_DETAILS
+from util import get_tables
 
 
 def main():
     db_details = DB_DETAILS['dev']
-    print(db_details)
+    # print(db_details)
+    tables = get_tables('table_list')
+    for table in tables['table_name']:
+        print(table)
 
 
 if __name__ == '__main__':
